e7d6dc931bdd8c7417ae53e4e785c652ac4a0981
Sharmila-Rajendran <139245927+Sharmila-Rajendran@users.noreply.github.com>
2024-07-21 12:06:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
-var AddTask=document.getElementById("add").value;
-var Addbutt=document.getElementById("butt1");
-var AddList=document.getElementById("add");
 function todolist(){
-    if(AddTask==="" || AddTask===null){
+    var AddTask=document.getElementById("add");
+    var ListTask=document.getElementById("newlist");
+    if(AddTask.value.trim()==="" || AddTask.value.trim()===null){
         alert("Please enter a task");
         return false;
     }
-    else{
-        document.getElementById("add").innerHTML="Sharmila will complete the task today";
-    }
-    
+
+    var tasks=document.createElement('div');
+    tasks.className="row";
+    tasks.innerHTML= 
+    `<div class="col-9">
+        <input type="text" name="tsks" class="list-value" value="${AddTask.value}" readonly>
+    </div>
+    <div class="col-3">
+        <button class="deletebutton" type="submit" onclick="deleteTask(this)">DELETE</button>
+    </div>`;
+    ListTask.appendChild(tasks);
+    AddTask.value="";
 }
 
-function DeleteTask(){
-    let listdelete=document.getElementById("tsk");
-    let deletebutton=document.getElementById("butt2");
-    listdelete.remove();
-    deletebutton.remove();
+function deleteTask(button){
+    var tasks=button.parentElement.parentElement;
+    tasks.remove();
 }
